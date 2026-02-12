0d05b1eb3ab6a4b2abaa405d646bffbebbd62a33
Sukhdeep <SukhdeepSingh1821@gmail.com>
2026-02-11 19:24:35 -0600

    Partial completed server.js  File

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3001
 app.use(express.static('public'))
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(_dirname), 'public', 'index.html')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.listen(port, ()=> {console.log(`Server is running on port ${port}`)})
